881209565a39faab348b54936c26270344d3375a
psychedel <orion.bytes@gmail.com>
2022-08-23 17:53:02 +0200

    Add anvil_extras submodule

---
+++ anvil-stubs/tables/app_tables.pyi
@@ -0,0 +1,17 @@
+from anvil.tables import Table
+
+progresses: Table
+reports: Table
+answertypes: Table
+questionnaires: Table
+dimensions: Table
+users: Table
+languages: Table
+companies: Table
+calculationlogics: Table
+projects: Table
+departments_2: Table
+questions: Table
+graphtypes: Table
+groups: Table
+sotest: Table
